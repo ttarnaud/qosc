3f7ae9c5bdd74ade01b50e58040c4c11df03862d
ttarnaud <thomas.tarnaud@ugent.be>
2019-08-13 16:57:45 +0200

    small changes

--- SONIC_TC.m
+++ SONIC_TC.m
@@ -1,6 +1,5 @@
-function Out = SimplNICETCLU(ESi,DISPLAY,tNICE,t,Q,m,n,h,s,u,w,wLock,hProtein,cCai,...
-    CmR,Gna,Vna,Gk,Vk,GT,fVCa,Gl,Vl,GKL,Gh,ginc,Vh,am,ampbm,an,anpbn,ah,...
-    ahpbh,sinf,taus,uinf,tauu,winf,tauw,k1,k2,k3,k4,Far,deffCa,tauCa,VLIMs)
+function Out = SONIC_TC(ESi,USPaT,DISPLAY,tNICE,t,Q,m,n,h,s,u,w,wLock,hProtein,cCai,...
+    Gna,Vna,Gk,Vk,GT,fVCa,Gl,Vl,GKL,Gh,ginc,Vh,k1,k2,k3,k4,Far,deffCa,tauCa,f1Veff0,f1VeffPa,f1rt0,f1rtPa)
 if DISPLAY == 2
 global reverseStr; %#ok<TLEV>
 Progress = 100*(t-tNICE(1))/(tNICE(2)-tNICE(1));  %#ok<*NASGU>
@@ -8,17 +7,18 @@ msg = sprintf('Progress: %3.1f', Progress);
 fprintf([reverseStr, msg]);
 reverseStr = repmat(sprintf('\b'), 1, length(msg)); 
 end
-m = m*(m<=1)+(m>1); 
-n = n*(n<=1)+(n>1);
-h = h*(h<=1)+(h>1);
-s = s*(s<=1)+(s>1);
-u = u*(u<=1)+(u>1);
-w = w*(w<=1)+(w>1);
+rate = struct;
+rate.('m') = m*(m<=1)+(m>1); 
+rate.('n') = n*(n<=1)+(n>1);
+rate.('h') = h*(h<=1)+(h>1);
+rate.('s') = s*(s<=1)+(s>1);
+rate.('u') = u*(u<=1)+(u>1);
+rate.('w') = w*(w<=1)+(w>1);
 
-V = 10^(3)*Q/CmR(t);
-if (V<VLIMs(1)||V>VLIMs(2)), error('Error: transmembrane potential outside interpolation interval!'); end
-Out = [ESi(t)-10^(-3)*(Gna*m^3*h*(V-Vna)+Gk*n^4*(V-Vk)+GT*s^2*u*(V-fVCa(cCai))+Gl*(V-Vl)+...
-    GKL*(V-Vk)+Gh*(w+ginc*wLock)*(V-Vh));
+if USPaT(t) == 0
+Veff = f1Veff0(Q);
+Out = [ESi(t)-10^(-3)*(Gna*m^3*h*(Veff-Vna)+Gk*n^4*(Veff-Vk)+GT*s^2*u*(Veff-fVCa(cCai))+Gl*(Veff-Vl)+...
+    GKL*(Veff-Vk)+Gh*(w+ginc*wLock)*(Veff-Vh));
 am(V)-(ampbm(V))*m;
 an(V)-(anpbn(V))*n;
 ah(V)-(ahpbh(V))*h;
--- SONICrun.m
+++ SONICrun.m
@@ -48,13 +48,6 @@ DISPLAY = 1;
 % DISPLAY = 0 -> No information displayed (use this option for HPC simulations)
 % DISPLAY = 1 -> Display progress based on update nr. alone
 
-Charges = 0; % This mode is meant for investigation on the mechanism of US-STN stimulation:
-% All charges are calculated, but the solver is slowed down
-if Charges == 1
-    disp('Note 3: Charge-mode is on for STN-mechanism investigation!');
-    disp(' ');
-end
-
 % Discretisation parameters
 dt = 50e-6;            % Discretisation time (s)
 atol = 1e-6; rtol = 1e-3; % absolute and relative VSVO-tolerances
@@ -352,19 +345,20 @@ end
 
 
 % 4. Solver
-global reverseStr; reverseStr = '';  %#ok<*TLEV>
+global reverseStr;  %#ok<*TLEV>
 if DISPLAY, disp('Solver started'); end
 SearchRange = [USibegin USiend]; PrecisionCheck = 0;
 if SearchMode % SearchMode = 1
 IIpa = (SearchRange(1)+SearchRange(2))/2; % W/m^2
 else % SearchMode = 0
 if ~any(SearchRange)
-IIpa = 1000;            % Don't choose too small, because it will slow down calculation -> Linear BLS is relatively slow
+IIpa = 1000;          
 else
 IIpa = SearchRange(~~SearchRange)*10^(3-2*find(SearchRange));
 end
 end
 while ~SearchMode || ~PrecisionCheck 
+    reverseStr = '';
 if MODE == 1
 USPaT = @(t) sqrt(2*rhol*c*IIpa)*USstep(t);
 elseif MODE == 2
@@ -387,69 +381,57 @@ elseif MODEL == 3 || MODEL == 8
 %-------------------THALAMOCORTICAL NEURONS--------------------------------
 %--------------------------------------------------------------------------
 elseif MODEL == 4
-[t,U] = ode15s(@(t,U) SONIC_TC(ESi,USPaT,USfreq,DISPLAY,tNICE,t,U(1),U(2),U(3),U(4),U(5),U(6),U(7),U(8),...
-    U(9),U(10),Gna,Vna,Gk,Vk,GT,fVCa,Gl,Vl,GKL,Gh,ginc,Vh,k1,k2,k3,k4,Far,deffCa,tauCa,f3Veff,f3rt),tNICE,Y0,OdeOpts);
+[t,U] = ode15s(@(t,U) SONIC_TC(ESi,USPaT,DISPLAY,tNICE,t,U(1),U(2),U(3),U(4),U(5),U(6),U(7),U(8),...
+    U(9),U(10),Gna,Vna,Gk,Vk,GT,fVCa,Gl,Vl,GKL,Gh,ginc,Vh,k1,k2,k3,k4,Far,deffCa,tauCa,...
+    f1Veff0,f1VeffPa,f1rt0,f1rtPa,SONICgates),tNICE,Y0,OdeOpts);
 %--------------------------------------------------------------------------
 %------------------NUCLEUS RETICULARIS NEURONS-----------------------------
 %--------------------------------------------------------------------------
 elseif MODEL == 5
-[t,U] = ode15s(@(t,U) SONIC_RE(ESi,USPaT,USfreq,DISPLAY,tNICE,t,U(1),U(2),U(3),U(4),U(5),U(6),U(7),...
-    Gna,Vna,Gk,Vk,GT,fVCa,Gl,Vl,Far,deffCa,tauCa,f3Veff,f3rt),tNICE,U0,OdeOpts);
+[t,U] = ode15s(@(t,U) SONIC_RE(ESi,USPaT,DISPLAY,tNICE,t,U(1),U(2),U(3),U(4),U(5),U(6),U(7),...
+    Gna,Vna,Gk,Vk,GT,fVCa,Gl,Vl,Far,deffCa,tauCa,f1Veff0,f1VeffPa,f1rt0,f1rtPa,SONICgates),tNICE,Y0,OdeOpts);
 % -------------------------------------------------------------------------
 % -----------------------SUBTHALAMIC NUCLEUS MODEL-------------------------
 % -------------------------------------------------------------------------
 elseif MODEL == 9
-if Charges
-[t,U] = ode113(@(t,U) SimplNICESTNLUcharges(ESi,DISPLAY,tNICE,t,...
-    U(1),U(2),U(3),U(4),U(5),U(6),U(7),U(8),U(9),U(10),U(11),U(12),U(13),...
-    U(14),U(15),U(16),U(17),U(18),U(19),U(20),...
-    CmR,Gna,Vna,Gk,Vk,Gl,Vl,GT,fVCa,GCa,GA,GL,minf,ninf,...
-    pinf,hinf,qinf,rinf,ainf,binf,cinf,d1inf,d2inf,taum,taun,taup,tauh,tauq,...
-    taur,taua,taub,tauc,taud1,taud2,Far,tauCa,VLIMs,kcCaiLIMs),tNICE,U0,OdeOpts); %#ok<*UNRCH>
-else
 OdeOpts=odeset('MaxStep',dt,'AbsTol',1e-7,'RelTol',1e-4);
-[t,U] = ode113(@(t,U) SimplNICESTNLU(ESi,DISPLAY,tNICE,t,...
+[t,U] = ode113(@(t,U) SONIC_STN(ESi,USPaT,DISPLAY,tNICE,t,...
     U(1),U(2),U(3),U(4),U(5),U(6),U(7),U(8),U(9),U(10),U(11),U(12),U(13),...
-    CmR,Gna,Vna,Gk,Vk,Gl,Vl,GT,fVCa,GCa,GA,GL,minf,ninf,...
-    pinf,hinf,qinf,rinf,ainf,binf,cinf,d1inf,d2inf,taum,taun,taup,tauh,tauq,...
-    taur,taua,taub,tauc,taud1,taud2,Far,tauCa,VLIMs,kcCaiLIMs),tNICE,U0,OdeOpts);
-end
+    Gna,Vna,Gk,Vk,Gl,Vl,GT,fVCa,GCa,GA,GL,Far,tauCa,f1Veff0,f1VeffPa,f1rt0,f1rtPa,SONICgates),tNICE,Y0,OdeOpts);
 % -------------------------------------------------------------------------
 % -----------------------THALAMUS RUBIN-TERMAN MODEL-----------------------
 % -------------------------------------------------------------------------
 elseif MODEL == 10
-[t,U] = ode113(@(t,U) SONIC_ThRT(ESi,USPaT,USfreq,DISPLAY,tNICE,t,U(1),U(2),U(3),Gna,Vna,...
-    Gk,Vk,Gl,Vl,GT,VT,f3Veff,f3rt),tNICE,U0,OdeOpts);
+[t,U] = ode113(@(t,U) SONIC_ThRT(ESi,USPaT,DISPLAY,tNICE,t,U(1),U(2),U(3),Gna,Vna,...
+    Gk,Vk,Gl,Vl,GT,VT,f1Veff0,f1VeffPa,f1rt0,f1rtPa,SONICgates),tNICE,Y0,OdeOpts);
 % -------------------------------------------------------------------------
 % ----------------------GLOBUS PALLIDUS INTERNUS NUCLEUS ------------------
 % -------------------------------------------------------------------------
 elseif MODEL == 11
-[t,U] = ode113(@(t,U) SimplNICEGPiLU(ESi,DISPLAY,tNICE,t,U(1),U(2),U(3),U(4),U(5),...
-    CmR,Gna,Vna,Gk,Vk,Gl,Vl,GT,VT,GCa,VCa,Gahp,minf,ainf,sinf,ninf,hinf,rinf,...
-    taun,tauh,taur,VLIMs),...
-    tNICE,U0,OdeOpts);
+[t,U] = ode113(@(t,U) SONIC_GPi(ESi,USPaT,DISPLAY,tNICE,t,U(1),U(2),U(3),U(4),U(5),...
+    Gna,Vna,Gk,Vk,Gl,Vl,GT,VT,GCa,VCa,Gahp,f1Veff0,f1VeffPa,f1rt0,f1rtPa,SONICgates),...
+    tNICE,Y0,OdeOpts);
 % -------------------------------------------------------------------------
 % ----------------------GLOBUS PALLIDUS EXTERNUS NUCLEUS-------------------
 % -------------------------------------------------------------------------
 elseif MODEL == 12
-[t,U] = ode113(@(t,U) SimplNICEGPeLU(ESi,DISPLAY,tNICE,t,U(1),U(2),U(3),U(4),U(5),...
-    CmR,Gna,Vna,Gk,Vk,Gl,Vl,GT,VT,GCa,VCa,Gahp,minf,ainf,sinf,ninf,hinf,rinf,...
-    taun,tauh,taur,VLIMs),...
-    tNICE,U0,OdeOpts);
+[t,U] = ode113(@(t,U) SONIC_GPe(ESi,USPaT,DISPLAY,tNICE,t,U(1),U(2),U(3),U(4),U(5),...
+    Gna,Vna,Gk,Vk,Gl,Vl,GT,VT,GCa,VCa,Gahp,f1Veff0,f1VeffPa,f1rt0,f1rtPa,SONICgates),...
+    tNICE,Y0,OdeOpts);
 % -------------------------------------------------------------------------
 % -----------------------MEDIUM SPINY STRIATUM NEURONS---------------------
 % -------------------------------------------------------------------------
 elseif MODEL == 13
-[t,U] = ode113(@(t,U) SimplNICEMSNLU(ESi,DISPLAY,tNICE,t,U(1),U(2),U(3),U(4),U(5),...
-    CmR,Gna,Vna,Gk,Vk,Gl,Vl,Vm,am,ampbm,an,anpbn,ap,appbp,ah,ahpbh,VLIMs),tNICE,U0,OdeOpts);
+[t,U] = ode113(@(t,U) SONIC_MSN(ESi,USPaT,DISPLAY,tNICE,t,U(1),U(2),U(3),U(4),U(5),...
+    Gna,Vna,Gk,Vk,Gl,Vl,Vm,f1Veff0,f1VeffPa,f1rt0,f1rtPa,SONICgates),tNICE,Y0,OdeOpts);
 % -------------------------------------------------------------------------
 % --------------------------HODGKIN-HUXLEY NEURONS-------------------------
 % -------------------------------------------------------------------------
 elseif MODEL == 14
-[t,U] = ode113(@(t,U) SimplNICEHHLU(ESi,DISPLAY,tNICE,t,U(1),U(2),U(3),U(4),...
-    CmR,Gna,Vna,Gk,Vk,Gl,Vl,am,ampbm,an,anpbn,ah,ahpbh,VLIMs),tNICE,U0,OdeOpts);
+[t,U] = ode113(@(t,U) SONIC_HH(ESi,USPaT,DISPLAY,tNICE,t,U(1),U(2),U(3),U(4),...
+    Gna,Vna,Gk,Vk,Gl,Vl,f1Veff0,f1VeffPa,f1rt0,f1rtPa,SONICgates),tNICE,Y0,OdeOpts);
 end
-TvaluesY = t;
+TvaluesY = t; Y = U;
 
 APindex = (10^5*U(:,1)>Qthresh)&(circshift(10^5*U(:,1),1,1)<Qthresh);
 APindex(1) = 0; % Remove circshift artifact
@@ -483,7 +465,7 @@ SaveStr2=['Chargevt(' MODELstr ')-Tsim=' num2str(Tsim) '-US(' num2str(USpstart) 
         num2str(USfreq) ',' num2str(USdc) ',' num2str(USprf) ',' USisppa ...
         ')-ES(' num2str(ESpstart) ',' num2str(ESpd) ',' num2str(ESdc) ',' ...
         num2str(ESprf) ',' ESisppa ').mat'];
-saveChargeSample = [TvaluesY', 10^5*Y(:,1)];
+saveChargeSample = [TvaluesY, 10^5*Y(:,1)];
 save(SaveStr2,'saveChargeSample');
 break;
 elseif MODE == 1
@@ -502,11 +484,13 @@ else
 IIpa = IIpa*10^(3-2*find(SearchRange));
 end
 end
+disp(' ');
 Checkpoint=['CP(' MODELstr ')-Tsim=' num2str(Tsim) '-US(' num2str(USpstart) ',' num2str(USpd) ',' ...
         num2str(USfreq) ',' num2str(USdc) ',' num2str(USprf) ',' USisppa ...
         ')-ES(' num2str(ESpstart) ',' num2str(ESpd) ',' num2str(ESdc) ',' ...
         num2str(ESprf) ',' ESisppa '):' num2str(SearchRange)];
 disp(Checkpoint);   
+fprintf('\n'); 
 end
 end
 if MODE == 1
@@ -538,22 +522,8 @@ saveData.Y = Y;
 save(SaveDataStr,'saveData','-v7.3');
 end
 if PLOT == 1
-if MemorySaveMode == 3
-TvaluesYms = 10^(3)*TvaluesY'; % [ms]
 figure;
-hold on;
-plot(TvaluesYms,Charge);
-ylabel('Charge [nC/cm^2]');
-xlabel('Time [ms]');
-set(gca,'fontsize',17);
-hold off;
-else
-figure;
-GateNames = {'m','n','p','h','q','r','a','b','c','d1','d2','Ca'};
 nrGates = size(Y,2)-1; 
-if Charges
-    nrGates = nrGates-7;
-end
 TvaluesYms = 10^(3)*TvaluesY'; % [ms]
 
 subplot(1+ceil(nrGates/2),1,1);
@@ -566,10 +536,10 @@ subplot(1+ceil(nrGates/2),1,i+1);
 hold on;
 yyaxis left;
 plot(TvaluesYms,Y(:,2*i));
-ylabel(GateNames{2*i-1});
+ylabel(SONICgates{2*i-1});
 yyaxis right;
 plot(TvaluesYms,Y(:,2*i+1));
-ylabel(GateNames{2*i});
+ylabel(SONICgates{2*i});
 hold off;
 set(gcf,'color','w');
 end
@@ -577,273 +547,12 @@ if mod(nrGates,2)==1
 subplot(1+ceil(nrGates/2),1,1+ceil(nrGates/2));
 hold on;
 plot(TvaluesYms,Y(:,end));
-ylabel(GateNames{nrGates});
+ylabel(SONICgates{nrGates});
 hold off;
 end
 xlabel('Time [ms]');
 set(findall(gcf,'-property','FontSize'),'FontSize',14)
-if MemorySaveMode == 2 && 1
-figure;
-if exist('MemX') %#ok<*EXIST>
-CapacitanceUS = zeros(length(MemX),1);
-for i=1:length(MemX), CapacitanceUS(i) = Cm(MemX(i)); end
-Capacitance = [Cm0*ones(InTbb-1,1); CapacitanceUS; Cm0*ones(length(TvaluesYms)-InTee,1)];
-else
-Capacitance = Cm0;
-end
-Potential = 0.01*Charge./Capacitance; % [mV]
-plot(TvaluesYms,Potential);
-ylabel('Potential [mV]');
-xlabel('Time [ms]');
-set(gca,'fontsize',17);
-set(gcf,'color','w'); box off;
-if exist('MemX')
-figure;
-plot(TvaluesYms(InTbb:InTee),10^(9)*MemX);
-xlabel('Time [ms]');
-ylabel('Displacement [nm]')
-set(gca,'fontsize',17);
-set(gcf,'color','w'); box off;
-end
-figure;
-plot(TvaluesYms,100*Capacitance);
-xlabel('Time [ms]');
-ylabel('Capacitance [\muF/cm^2]');
-set(gca,'fontsize',17);
-set(gcf,'color','w'); box off;
-% ------------------ PLOT CURRENTS FOR SUBTHALAMIC NUCLEUS----------------
-if MODEL == 9
-mGate = Y(:,2); nGate = Y(:,3); pGate = Y(:,4); hGate = Y(:,5);
-qGate = Y(:,6); rGate = Y(:,7); aGate = Y(:,8); bGate = Y(:,9); cGate = Y(:,10);
-d1Gate = Y(:,11); d2Gate = Y(:,12); CaVal = Y(:,13);
-
-iNaVal = 0.1*Gna*mGate.^3.*hGate.*(Potential-Vna); % Na-current [uA/cm^2]
-iKVal = 0.1*Gk*nGate.^4.*(Potential-Vk);    % K-current [uA/cm^2]
-iTVal = 0.1*GT*pGate.^2.*qGate.*(Potential-fVCa(CaVal)); % T-current [uA/cm^2]
-ilVal = 0.1*Gl.*(Potential-Vl);         % Leak-current [uA/cm^2]
-iKCaVal = 0.1*GCa.*rGate.^2.*(Potential-Vk); % Ca-dependent K-current [uA/cm^2]
-iAVal = 0.1*GA*aGate.^2.*bGate.*(Potential-Vk); % A-type K-current [uA/cm^2]
-iLVal = 0.1*GL*cGate.^2.*d1Gate.*d2Gate.*(Potential-fVCa(CaVal)); % L-current [uA/cm^2]
-itotVal = 0.1*(Gl*(Potential-Vl)+Gna*mGate.^3.*hGate.*(Potential-Vna)+Gk*nGate.^4.*(Potential-Vk)+...
-   GT*pGate.^2.*qGate.*(Potential-fVCa(CaVal))+GCa.*rGate.^2.*(Potential-Vk)+...
-   GA*aGate.^2.*bGate.*(Potential-Vk)+GL*cGate.^2.*d1Gate.*d2Gate.*(Potential-fVCa(CaVal)));
-% Total current [uA/cm^2]
-if USpstart < Tsim && USpstart+USpd > 0 && Charges
-    QNa = 10^5*Y(:,14);  % [nC/cm^2]
-    QK = 10^5*Y(:,15);
-    QT = 10^5*Y(:,16);
-    QL = 10^5*Y(:,17);
-    QKCa = 10^5*Y(:,18);
-    QA = 10^5*Y(:,19);
-    Ql = 10^5*Y(:,20);
-BoolRange = TvaluesYms>=10^3*max(USpstart,0)&TvaluesYms<=10^3*min((USpstart+USpd),Tsim);
-TvaluesYmsR = TvaluesYms(BoolRange);
-
-QNa = QNa(BoolRange); QK = QK(BoolRange);
-QT = QT(BoolRange); Ql = Ql(BoolRange);
-QKCa = QKCa(BoolRange); QA = QA(BoolRange);
-QL = QL(BoolRange); 
-QNa = QNa-QNa(1); QK = QK-QK(1); QT = QT-QT(1);
-Ql = Ql-Ql(1); QKCa = QKCa-QKCa(1); QA = QA-QA(1);
-QL = QL-QL(1);
-Qtot = QNa+QK+QT+Ql+QKCa+QA+QL;
-
-% % Displaying all end charges to research mechanisms (uncomment)
-% [QNa(end), QK(end), QT(end), Ql(end), QKCa(end), QA(end), ...
-% QL(end), Qtot(end), QNa(end)+QK(end), ...
-% QT(end)+Ql(end)+QL(end)+QA(end)+QKCa(end)]
-
-figure;
-subplot(4,2,1);
-hold on;
-plot(TvaluesYmsR,QNa);
-ylabel('Q_{Na} [nC/cm^2]');
-hold off;
-subplot(4,2,2);
-hold on;
-plot(TvaluesYmsR,QK);
-ylabel('Q_{K} [nC/cm^2]');
-hold off;
-subplot(4,2,3);
-hold on;
-plot(TvaluesYmsR,QT);
-ylabel('Q_T [nC/cm^2]');
-hold off;
-subplot(4,2,4);
-hold on;
-plot(TvaluesYmsR,QL);
-ylabel('Q_L [nC/cm^2]');
-hold off;
-subplot(4,2,5);
-hold on;
-plot(TvaluesYmsR,QKCa);
-ylabel('Q_{K-Ca} [nC/cm^2]');
-hold off;
-subplot(4,2,6);
-hold on;
-plot(TvaluesYmsR,QA);
-ylabel('Q_A [nC/cm^2]');
-hold off;
-subplot(4,2,7);
-hold on;
-plot(TvaluesYmsR,Ql);
-ylabel('Q_l [nC/cm^2]');
-xlabel('Time [ms]');
-hold off;
-subplot(4,2,8);
-hold on;
-plot(TvaluesYmsR,Qtot);
-ylabel('Q_{tot} [nC/cm^2]');
-xlabel('Time [ms]');
-hold off;
-set(gcf,'color','w');
-set(findall(gcf,'-property','FontSize'),'FontSize',14);
-
-figure;
-subplot(1,2,1);
-hold on;
-plot(TvaluesYmsR,QNa+QK);
-ylabel('Q_{Na}+Q_K [nC/cm^2]');
-xlabel('Time [ms]');
-hold off;
-subplot(1,2,2);
-hold on;
-plot(TvaluesYmsR,QT+Ql+QKCa+QA+QL);
-ylabel('Q_T+Q_l+Q_{KCa}+Q_A+Q_L [nC/cm^2]');
-xlabel('Time [ms]');
-hold off;
-set(gcf,'color','w');
-set(findall(gcf,'-property','FontSize'),'FontSize',17);
-end
-
-figure;
-hold on;
-subplot(2,2,1);
-yyaxis left;
-plot(TvaluesYms,iNaVal);
-ylabel('i_{Na} [\muA/cm^2]');
-yyaxis right;
-plot(TvaluesYms,iKVal);
-ylabel('i_{K} [\muA/cm^2]');
-subplot(2,2,2);
-yyaxis left;
-plot(TvaluesYms,iTVal);
-ylabel('i_T [\muA/cm^2]');
-yyaxis right;
-plot(TvaluesYms,iLVal);
-ylabel('i_L [\muA/cm^2]');
-subplot(2,2,3);
-yyaxis left;
-plot(TvaluesYms,iKCaVal);
-ylabel('i_{K-Ca} [\muA/cm^2]');
-yyaxis right;
-plot(TvaluesYms,iAVal);
-ylabel('i_A [\muA/cm^2]');
-xlabel('Time [ms]');
-subplot(2,2,4);
-yyaxis left;
-plot(TvaluesYms,ilVal);
-ylabel('i_l [\muA/cm^2]');
-yyaxis right;
-plot(TvaluesYms,itotVal);
-ylabel('i_{tot} [\muA/cm^2]');
-xlabel('Time [ms]');
-set(gcf,'color','w');
-set(findall(gcf,'-property','FontSize'),'FontSize',14)
-
-figure;
-subplot(4,2,1);
-hold on;
-plot(TvaluesYms,iNaVal);
-ylabel('i_{Na} [\muA/cm^2]');
-hold off;
-subplot(4,2,2);
-hold on;
-plot(TvaluesYms,iKVal);
-ylabel('i_{K} [\muA/cm^2]');
-hold off;
-subplot(4,2,3);
-hold on;
-plot(TvaluesYms,iTVal);
-ylabel('i_T [\muA/cm^2]');
-hold off;
-subplot(4,2,4);
-hold on;
-plot(TvaluesYms,iLVal);
-ylabel('i_L [\muA/cm^2]');
-hold off;
-subplot(4,2,5);
-hold on;
-plot(TvaluesYms,iKCaVal);
-ylabel('i_{K-Ca} [\muA/cm^2]');
-hold off;
-subplot(4,2,6);
-hold on;
-plot(TvaluesYms,iAVal);
-ylabel('i_A [\muA/cm^2]');
-hold off;
-subplot(4,2,7);
-hold on;
-plot(TvaluesYms,ilVal);
-ylabel('i_l [\muA/cm^2]');
-xlabel('Time [ms]');
-hold off;
-subplot(4,2,8);
-hold on;
-plot(TvaluesYms,itotVal);
-ylabel('i_{tot} [\muA/cm^2]');
-xlabel('Time [ms]');
-hold off;
-set(gcf,'color','w');
-set(findall(gcf,'-property','FontSize'),'FontSize',14)
 end
 end
 
-% % - To plot charge from already plotted currents --
-% axes = get(gcf,'children');
-% for i = 1:length(axes)
-% datastruct(i) = get(axes(length(axes)-i+1),'children');
-% end
-% for i = 1:length(datastruct)
-% xdata{i} = get(datastruct(i),'XData');
-% ydata{i} = get(datastruct(i),'YData');
-% end
-% ydata2 = cellfun(@(ydata,xdata) ydata(xdata>=1000&xdata<=2000),ydata,xdata,'UniformOutput',false);
-% xdata2 = cellfun(@(xdata) xdata(xdata>=1000&xdata<=2000),xdata,'UniformOutput',false);
-% Dxdata = cellfun(@(xdata) circshift(xdata,-1,2)-xdata,xdata2,'UniformOutput',false);
-% Dxdata = cellfun(@(Dxdata) Dxdata(1:end-1),Dxdata,'UniformOutput',false);
-% charge = cellfun(@(ydata2,Dxdata) [0 cumsum(ydata2(1:end-1).*Dxdata)],ydata2,Dxdata,'UniformOutput',false);
-% figure;
-% for i = 1:length(xdata2)
-% subplot(4,2,i);
-% plot(xdata2{i},charge{i});
-% end
-
-% ------------------ PLOT CURRENTS FOR HH MODEL --------------------------
-if MODEL == 14
-mGate = Y(:,2); nGate = Y(:,3); hGate = Y(:,4); 
-iNaVal = 0.1*Gna*mGate.^3.*hGate.*(Potential-Vna); % Na-current [uA/cm^2]
-iKVal = 0.1*Gk*nGate.^4.*(Potential-Vk);    % K-current [uA/cm^2]
-ilVal = 0.1*Gl.*(Potential-Vl);         % Leak-current [uA/cm^2]
-itotVal = iNaVal+iKVal+ilVal;           % Total current [uA/cm^2]
-figure; hold on;
-subplot(2,2,1);
-plot(TvaluesYms,iNaVal);
-ylabel('i_{Na} [\muA/cm^2]'); xlabel('time [ms]');
-subplot(2,2,2);
-plot(TvaluesYms,iKVal);
-ylabel('i_{K} [\muA/cm^2]'); xlabel('time [ms]');
-subplot(2,2,3);
-plot(TvaluesYms,ilVal);
-ylabel('i_l [\muA/cm^2]'); xlabel('time [ms]');
-subplot(2,2,4);
-plot(TvaluesYms,itotVal); xlabel('time [ms]');
-ylabel('i_{tot} [\muA/cm^2]'); set(gcf,'color','w');
-title(['Total membrane current. Injected current (' num2str(100*ESipa) ' \muA/cm^2) excluded']);
-set(findall(gcf,'-property','FontSize'),'FontSize',14);
-hold off;
-end
-end
-end
-end
 
