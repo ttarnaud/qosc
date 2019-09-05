1aba330b644a2db27b270161e1d77ebc3849a972
ttarnaud <thomas.tarnaud@ugent.be>
2019-09-05 14:34:27 +0200

    Small bugfix for funtauu-function

--- funtauu.m
+++ funtauu.m
@@ -1,7 +1,7 @@
 function Out = funtauu(V,Vx)
     temp = (1/3.7)*(exp(-(V+Vx+22)/10.5)+28);
     mask = (V+Vx) < -80;
-    temp(mask) =  (1/3.7)*exp((V(mask)+Vx(mask)+467)/66.6); 
+    temp(mask) =  (1/3.7)*exp((V(mask)+Vx+467)/66.6); 
     Out = temp;
 % if (V+Vx) < -80
 %     Out = (1/3.7)*exp((V+Vx+467)/66.6);
