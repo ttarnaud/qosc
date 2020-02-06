016956ebb2d9b0df17734188eeea049f74292069
ttarnaud <thomas.tarnaud@ugent.be>
2020-02-06 18:33:33 +0100

    Previous assumption : first phase phi_1 can be taken 0 by convention. This is wrong, because phi_1 is already relative to the phase of the ultrasonic pressure, which is already taken 0 by convention. This assumption is now omitted in SONICtabulate.m

--- SONICtabulate.m
+++ SONICtabulate.m
@@ -5,41 +5,46 @@ ModelN = 1;
 aBLSRange = [16,32,64]*10^(-9); 		% Sonophore radius (m)
 USfreqRange = [0.02, 0.1, 0.5, 1, 2, 3, 4]*10^(6); % Ultrasonic frequency (Hz)
 USPaRange = [0, logspace(log10(100),log10(600000),50)];  % Ultrasonic pressure (Pa)
-fQmRange = @(Qm0) (Qm0-25*10^(-5):1*10^(-5):50*10^(-5)); % Membrane charge (C/m^2)
+fQmRange = @(Qm0) (Qm0-25*10^(-5):5*10^(-5):50*10^(-5)); % Membrane charge (C/m^2)
 fBLSRange = 1;                                           %#ok<*NASGU> % Membrane coverage
 psiQRange = 0;
 DeltaQmRange = 0;
 saveStrAdd = '';
 
 % DeltaQmRange tables (outcomment for normal tables)
-% DeltaQmRange = (0*10^(-5):25*10^(-5):500*10^(-5));  
-% USfreqRange = 0.5*10^(6);            % Ultrasonic frequency [Hz]
-% aBLSRange = 32*10^(-9);              % Sonophore radius (m)  
-% saveStrAdd = [saveStrAdd,'-Qosc'];
+DeltaQmRange = (0*10^(-5):25*10^(-5):500*10^(-5));  
+USfreqRange = 0.5*10^(6);            % Ultrasonic frequency [Hz]
+aBLSRange = 32*10^(-9);              % Sonophore radius (m)  
+saveStrAdd = [saveStrAdd,'-Qosc'];
 
 % Fourier oscillating tables (outcomment for normal tables)
-% Nfourier = 8;
-% NfourierIn = 1;
-% DeltaQmRange = (0*10^(-5):25*10^(-5):500*10^(-5));  
-% psiQRange = 0;
-% USPaRange = 100e3;
-% USfreqRange = 0.5*10^(6);            % Ultrasonic frequency [Hz]
-% aBLSRange = 32*10^(-9);              % Sonophore radius (m)  
-% saveStrAdd = [saveStrAdd,'Fourier' num2str(Nfourier) '-FourierIn' num2str(NfourierIn)];
+Nfourier = 2;
+NfourierIn = 2;
+DeltaQmRange = (0*10^(-5):25*10^(-5):100*10^(-5));  
+psiQRange = linspace(0,2*pi,10); psiQRange = psiQRange(1:end-1);
+USPaRange = 100e3;
+USfreqRange = 0.5*10^(6);            % Ultrasonic frequency [Hz]
+aBLSRange = 32*10^(-9);              % Sonophore radius (m)  
+saveStrAdd = [saveStrAdd,'Fourier' num2str(Nfourier) '-FourierIn' num2str(NfourierIn)];
 
 % For Fig. 10 from Lemaire et al. we need tables with multiple membrane
 % coverages -- Out-comment for normal tables.
-fBLSRange = (0.05:0.05:1);
-USfreqRange = 0.5*10^(6);            % Ultrasonic frequency [Hz]
-aBLSRange = 32*10^(-9);              % Sonophore radius (m)   
-saveStrAdd = [saveStrAdd '-xfs'];
+% fBLSRange = (0.05:0.05:1);
+% USfreqRange = 0.5*10^(6);            % Ultrasonic frequency [Hz]
+% aBLSRange = 32*10^(-9);              % Sonophore radius (m)   
+% saveStrAdd = [saveStrAdd '-xfs'];
 
 % Correct for charge redistribution if fBLS < 1 in the electrostatic force (corrPec) 
 corrPec = 0;                        % Bool: if 1, the table-name will contain "-corrPec"
 % --------------------------------------------------------------------------------
 deltaQmGrid = cell(NfourierIn,1);
-psiQGrid = cell(NfourierIn-1,1);            % First phase is zero by convention.
+psiQGrid = cell(NfourierIn,1);            % First phase is zero by convention.
 [deltaQmGrid{:}] = ndgrid(DeltaQmRange); [psiQGrid{:}] = ndgrid(psiQRange); 
+
+% c = parcluster('local');
+% p = parpool(c.NumWorkers);
+% disp('Parallel on cluster with number of workers:');
+% disp(c.NumWorkers);
 
 ME = ''; futIND = 0;
 try p = gcp;
@@ -73,8 +78,8 @@ switch MODEL
    case 14, MODELstr='HH'; Vm0 = -70;
 end
 QmRange = fQmRange(10^(-3)*Vm0*Cm0);
-SONICInitM = zeros(numel(QmRange),numel(USPaRange),numel(USfreqRange),numel(aBLSRange),numel(fBLSRange),numel(DeltaQmRange)^NfourierIn,numel(psiQRange)^(NfourierIn-1));
-SONICInitC = cell(numel(QmRange),numel(USPaRange),numel(USfreqRange),numel(aBLSRange),numel(fBLSRange),numel(DeltaQmRange)^NfourierIn,numel(psiQRange)^(NfourierIn-1));
+SONICInitM = zeros(numel(QmRange),numel(USPaRange),numel(USfreqRange),numel(aBLSRange),numel(fBLSRange),numel(DeltaQmRange)^NfourierIn,numel(psiQRange)^(NfourierIn));
+SONICInitC = cell(numel(QmRange),numel(USPaRange),numel(USfreqRange),numel(aBLSRange),numel(fBLSRange),numel(DeltaQmRange)^NfourierIn,numel(psiQRange)^(NfourierIn));
 SONICtable = struct('Zeff',SONICInitM,'Veff',SONICInitM,'ngend',SONICInitM,'Cmeff',SONICInitM,'ampV',SONICInitM,'DeltaPhi',SONICInitM);
 SONICtable.('cfit') = SONICInitC; SONICtable.('gof') = SONICInitC; 
 % SONICtable.('output') = SONICInitC;   Saving output details is extremely
@@ -82,10 +87,10 @@ SONICtable.('cfit') = SONICInitC; SONICtable.('gof') = SONICInitC;
 
 ProgressIncr1 = 0; ProgressIncr2 = 0; reverseStr1 = ''; reverseStr2 = '';
 if ParallelON == 1
-for ipsiQ = (1:length(psiQRange)^(NfourierIn-1))
+for ipsiQ = (1:length(psiQRange)^NfourierIn)
 psiQ = zeros(NfourierIn,1);
-for ifr = 1:NfourierIn-1
-psiQ(ifr+1) = psiQGrid{ifr}(ipsiQ);
+for ifr = 1:NfourierIn
+psiQ(ifr) = psiQGrid{ifr}(ipsiQ);
 end
     for iDeltaQm = (1:length(DeltaQmRange)^NfourierIn)
     DeltaQm = zeros(NfourierIn,1);    
@@ -120,10 +125,10 @@ end
 end
 end
 fprintf('\n');
-for ipsiQ = (1:length(psiQRange)^(NfourierIn-1))
+for ipsiQ = (1:length(psiQRange)^NfourierIn)
 psiQ = zeros(NfourierIn,1);
-for ifr = 1:NfourierIn-1
-psiQ(ifr+1) = psiQGrid{ifr}(ipsiQ);
+for ifr = 1:NfourierIn
+psiQ(ifr) = psiQGrid{ifr}(ipsiQ);
 end
     for iDeltaQm = (1:length(DeltaQmRange)^NfourierIn)
     DeltaQm = zeros(NfourierIn,1);
